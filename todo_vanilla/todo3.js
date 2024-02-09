1088efca0988d85ed0e8c697bd7fc7e9d73e3cbc
jinjaehwan <jjh099@icloud.com>
2024-02-10 01:05:59 +0900

    todo3 삭제까지

---
+++ todo_vanilla/todo3.js
@@ -0,0 +1,62 @@
+const todoList = document.getElementById("todoList");
+const inputTodo = document.getElementById("inputTodoText");
+
+// todo를 담을 변수
+let todos = [];
+let id = 0;
+
+const createTodo = () => {
+  document.getElementById("todoForm").addEventListener("submit", (e) => {
+    e.preventDefault();
+    onAdd(inputTodo.value);
+    inputTodo.value = "";
+    console.log(todos);
+    onRender();
+  });
+};
+
+const onRender = () => {
+  todoList.innerHTML = "";
+  todos.map((todo) => {
+    const todoItem = document.createElement("li");
+    todoItem.dataset.id = todo.id;
+
+    const todoText = document.createElement("p");
+    todoText.textContent = todo.text;
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "삭제";
+    deleteButton.addEventListener("click", () => {
+      onDelete(todo.id);
+      onRender();
+    });
+
+    const updateButton = document.createElement("button");
+    updateButton.textContent = "완료";
+    todoItem.appendChild(updateButton);
+    todoItem.appendChild(todoText);
+    todoItem.appendChild(deleteButton);
+    todoList.appendChild(todoItem);
+  });
+};
+// todo 추가
+const onAdd = (todoText) => {
+  if (todoText.trim() !== "") {
+    todos.push({
+      id: id++,
+      text: todoText,
+      complete: false,
+    });
+  } else {
+    alert("할 일을 입력해주세요!");
+  }
+};
+// todo 삭제
+const onDelete = (id) => {
+  todos = todos.filter((todo) => todo.id !== id);
+};
+
+const onUpdateTodo = (id) => {};
+
+createTodo();
+onRender();
